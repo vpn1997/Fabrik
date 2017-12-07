57d0c0b1f395a10f1238e24f067d3730bb8bbb8f
VIPIN SINGH <mailme.vipin475@gmail.com>
2017-12-07 12:38:20 +0530

    Update content.js

--- ide/static/js/content.js
+++ ide/static/js/content.js
@@ -717,6 +717,10 @@ class Content extends React.Component {
                         <h3 className="zoo-modal-text">Caption</h3>
                         <ModelElement importNet={this.importNet} framework="caffe" id="CoCo_Caption">CoCo Caption</ModelElement>
                         </div>
+                        <div className="zoo-modal-model">
+                        <h3 className="zoo-modal-text">VQA</h3>
+                        <ModelElement importNet={this.importNet} framework="keras" id="VQA">VQA</ModelElement>
+                        </div>
                         </div>
                         </div>
 
